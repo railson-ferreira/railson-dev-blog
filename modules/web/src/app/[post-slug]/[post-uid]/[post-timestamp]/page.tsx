fdec08f1bd3a0ce0299b75eb18d065d7552453e6
Railson Ferreira <railson.info@gmail.com>
2023-11-09 02:44:14 +0000

    refactor: Improve SEO and fix Markdown render

--- modules/web/src/app/[post-slug]/[post-uid]/[post-timestamp]/page.tsx
+++ modules/web/src/app/[post-slug]/[post-uid]/[post-timestamp]/page.tsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import {gql, GraphQLClient} from "graphql-request";
-import Markdown from "react-markdown";
+import Markdown from "markdown-to-jsx";
 import {notFound} from "next/navigation";
-import Seo from "@/app/[post-slug]/[post-uid]/[post-timestamp]/seo";
+import {Metadata} from "next";
 
-export default async function Page({params}: any) {
+export async function generateMetadata(
+  { params }: any
+): Promise<Metadata> {
   const data: any = await fetchData(params["post-uid"], params["post-timestamp"]);
   if (data.post == null) {
-    notFound()
+    return {}
   }
   const {uid, timestamp, filePath, contentMarkdown} = data.post
   const title = contentMarkdown.match(/title: (.+)/)[1];
   const slug = filePath.replace(".md", "");
+  return {
+    title: title,
+    openGraph: {
+      type: 'article',
+      title: title,
+      url: `https://blog.railson.dev/${slug}/${uid}/${timestamp}`,
+      images: ['/seo.png'],
+      publishedTime: new Date(Number(uid)*1000).toISOString(),
+      modifiedTime: new Date(Number(timestamp)*1000).toISOString(),
+      authors: [
+        'https://railson.dev'
+      ],
+      tags: [
+        // 'Tag A', 'Tag B', 'Tag C'
+      ],
+    },
+  }
+}
+export default async function Page({params}: any) {
+  // TODO: verify cache
+  const data: any = await fetchData(params["post-uid"], params["post-timestamp"]);
+  if (data.post == null) {
+    notFound()
+  }
+  const { contentMarkdown} = data.post
   return (
     <>
-      <Seo
-        uid={uid}
-        timestamp={timestamp}
-        title={title}
-        slug={slug}
-      />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <Markdown>{contentMarkdown.replace(/---(.|\n)+---/g, "").replaceAll(/<!--.+-->/g, "")}</Markdown>
+          <Markdown>{contentMarkdown.replace(/---(.|\n)+---/g, "")}</Markdown>
         </div>
       </main>
     </>
--- modules/web/src/app/layout.tsx
+++ modules/web/src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Railson Dev Blog',
   description: 'Made with ☕ by Rai',
+  openGraph: {
+    images: ['/seo.png'],
+  },
 }
 
 export default function RootLayout({
